a64c5fa50273ab76c3600a2777a86adc1d742c3f
NevNeveru <62367190+NevNeveru@users.noreply.github.com>
2025-02-26 09:32:22 +0700

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault();
     alert('Thank you for contacting us! We will get back to you soon.');
-    // You can add more functionality here, like sending the form data to a server.
+    // Clear the form
+    document.getElementById('contact-form').reset();
 });
